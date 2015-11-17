5680a736e62742c774fcfc91112efa7ce88c191a
Austin Bailey <baileyaustinw@gmail.com>
2015-11-17 13:12:34 -0500

    Basic error reporting

--- client/templates/posts/posts_list.js
+++ client/templates/posts/posts_list.js
@@ -3,5 +3,8 @@ Template.postsList.helpers({
     return Posts.find({}, {
       sort: {submitted: -1}
     });
+  },
+  postsCount: function() {
+    return Posts.find().count() > 0;
   }
 });
--- server/fixtures.js
+++ server/fixtures.js
@@ -1,4 +1,4 @@
-if (Posts.find().count() === 0) {
+/*if (Posts.find().count() === 0) {
   Posts.insert({
     title: 'Introducing Telescope',
     url: 'http://sachagreif.com/introducing-telescope'
@@ -14,3 +14,4 @@ if (Posts.find().count() === 0) {
     url: 'http://themeteorbook.com'
   });
 }
+*/
